a2c17aa963fd690917c817c2e9e281b5074d067a
anindita210993 <aninditamazumder802@gmail.com>
2025-09-13 16:28:58 +0530

    Add files via upload

---
+++ week1/day1/Chrome.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		float versionOfType=91.0f;
+		String developerOfType="Google";
+		boolean betaOfType=false;
+		int yearOfType=2008;
+		char shortcutKeyOfType='C';
+		System.out.println("Version type is "+versionOfType +"\nDeveloper type is "+developerOfType +
+				"\nBeta type is "+betaOfType +"\nYear type is "+yearOfType +"\nShortcut key type is "+shortcutKeyOfType);
+		//System.out.println("Developer type is "+developerOfType);
+		//System.out.println("Beta type is "+betaOfType);
+		//System.out.println("Year type is "+yearOfType);
+		//System.out.println("Shortcut key type is "+shortcutKeyOfType);
+
+	}
+
+}
